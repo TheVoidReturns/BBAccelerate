e4ef32114097d5833964d93175c44d6f84da5764
Robin <robingpeel@gmail.com>
2016-05-28 11:19:33 +0100

    background art in, minor tidying on dice, beginning of event sequence class

--- src/com/scatterlogic/apps/bbaccelerate/EventSequenceActivity.java
+++ src/com/scatterlogic/apps/bbaccelerate/EventSequenceActivity.java
@@ -1,6 +1,7 @@
 package com.scatterlogic.apps.bbaccelerate;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Robin on 18/12/2015.
@@ -13,5 +14,20 @@ import android.app.Activity;
 
 public class EventSequenceActivity extends Activity {
     static String[] eventTypes = {"Pre-Match","Kick-Off","Post-Match","Armour, then Injury"};
+    protected void OnCreate(Bundle savedInstanceState){
+
+    }
+
+    protected void OnPause(){
+
+    }
+
+    protected void OnDestroy(){
+
+    }
+
+    protected void OnResume(){
+
+    }
 
 }

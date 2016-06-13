112387d023d329b8b8211086a93f6f106bfb151c
Robin <robingpeel@gmail.com>
2016-06-13 23:06:00 +0100

    Lots of moves.  Implementation of the Kick Off Sequence, tested and successful.

--- src/com/scatterlogic/apps/bbaccelerate/InGameActivity.java
+++ src/com/scatterlogic/apps/bbaccelerate/InGameActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 
 public class InGameActivity extends Activity
 {
+
 	Button[] allButtons = new Button[8];
 	String[] teamNames;
 	Button kickOffButton, pickUpButton, blockButton, throwInButton,passButton,
@@ -22,7 +23,7 @@ public class InGameActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.ingame);
 		assignXMLComponentsToVariables();
 		tidyUpLayout();
